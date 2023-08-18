759a5db1e3a72b29b8c1da9fabca92c18034e9e5
An Nguyen <navdevengr@gmail.com>
2023-08-18 12:50:18 -0500

    part5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 //destructure useState from React
 import { useState } from "react";
-
 // import component styles from App.css
 import "./App.css";
-
 // Import Square.js component
 import Square from "./Square";
 
@@ -12,18 +10,55 @@ function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   // Using array destructuring to initialize state of the player variable to a boolean of true
   const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  //calculates the winner
+  const calculateWinner = (arr) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        return `${arr[a]} won!`;
+      }
+    }
+    return "Who will win?";
+  };
+
   return (
     <div className="App">
-      <Square
-        //Add a prop of squares that is equal to the squares state variable to the Square component
-        squares={squares}
-        //Add a prop of setSquares that is equal to the setSquares function to the Square component
-        setSquares={setSquares}
-        //Add a prop of player that is equal to the player state variable to the Square component
-        player={player}
-        //Add a prop of setPlayer that is equal to the setPlayer function to the Square component
-        setPlayer={setPlayer}
-      />
+      <span>{calculateWinner(squares)}</span>
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+              //Add a prop of squares that is equal to the squares state variable to the Square component
+              squares={squares}
+              //Add a prop of setSquares that is equal to the setSquares function to the Square component
+              setSquares={setSquares}
+              //Add a prop of player that is equal to the player state variable to the Square component
+              player={player}
+              //Add a prop of setPlayer that is equal to the setPlayer function to the Square component
+              setPlayer={setPlayer}
+              squareValue={val}
+              index={index}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 const Square = (props) => {
-  // return <div className="square">{console.log(props.squares, props.player)}</div>;
-  //verify the props are being passed correctly by using console.log
-  console.log(props.squares, props.player)
+  const handleClick = () => {
+    //this function jamdles when a user clicks on a square
+    // first if statement checks if there is a value in props.squareValue ! is a "logical not" operator, so this checks if the square is "falsy" or empty.
+    if (!props.squareValue) {
+      if (props.player) {
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      } else {
+        props.squares.splice(props.index, 1, "O");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      }
+    }
+  };
+  return (
+    // invoke the handleClick function if user clicks on the div with className = "square"
+    <div className="square" onClick={handleClick}>
+      {props.squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
+    </div>
+  );
 };
 
 export default Square;
